e946db37d4fe942a1d14653bef95d3ddc56be38e
aldlss <ayaldlss@gmail.com>
2023-03-08 20:02:59 +0800

    add: homework1

---
+++ homework1/Array.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace homework1
+{
+    internal class Array : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private double maxx, minn, avg, sum;
+        private string list;
+        private Visibility error;
+
+        public Array()
+        {
+            maxx = minn =avg = sum = 0;
+            list = string.Empty;
+            error = Visibility.Collapsed;
+        }
+
+        private void ListHandle(string list)
+        {
+            string[] arr = list.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            if (arr.Length == 0)
+            {
+                Sum = Avg = Maxx = Minn = 0;
+                return;
+            }
+            double sum = 0;
+            int count = 0;
+            double maxx = double.MinValue, minn = double.MaxValue;
+            foreach (string s in arr)
+            {
+                double num;
+                bool ok = double.TryParse(s, out num);
+                if (!ok)
+                {
+                    Error = Visibility.Visible;
+                    return;
+                }
+                sum += num;
+                maxx = Double.MaxNumber(maxx, num);
+                minn = Double.MinNumber(minn, num);
+                ++count;
+            }
+
+            Error = Visibility.Collapsed;
+            Maxx = maxx;
+            Minn = minn;
+            Sum = sum;
+            Avg = sum / count;
+        }
+
+        public Visibility Error
+        {
+            get => error;
+            set
+            {
+                error = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string List
+        {
+            get => list;
+            set
+            {
+                list = value;
+                ListHandle(list);
+            }
+        }
+        public double Maxx
+        {
+            get => maxx;
+            set
+            {
+                if (Math.Abs(maxx - value) > 0.001)
+                {
+                    maxx = value;
+                    OnPropertyChanged();
+                }
+            }
+
+        }
+
+        public double Minn
+        {
+            get => minn;
+            set
+            {
+                if (Math.Abs(minn - value) > 0.001)
+                {
+                    minn = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double Avg
+        {
+            get => avg;
+            set
+            {
+                if (Math.Abs(avg - value) > 0.001)
+                {
+                    avg = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double Sum
+        {
+            get => sum;
+            set
+            {
+                if (Math.Abs(sum - value) > 0.001)
+                {
+                    sum = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ homework1/Prime.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace homework1
+{
+    internal class Prime : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private int up, down;
+        private string ans;
+
+        public Prime()
+        {
+            up = down = 0;
+            ans = string.Empty;
+        }
+
+        private static bool IsPrime(int num)
+        {
+            if (num == 2) return true;
+            if (num <= 1 || num % 2 == 0) return false;
+            for (int i = 3; i <= Double.Sqrt(num); i += 2)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void PrimeSearch()
+        {
+            int conut = 0;
+            StringBuilder sb = new();
+            for (int i = (down <= 1 ? 2 : down); i <= up; i++)
+            {
+                if (IsPrime(i))
+                {
+                    sb.AppendFormat("{0}, ", i);
+                    ++conut;
+                    if (conut == 10)
+                    {
+                        sb.AppendLine("");
+                        conut = 0;
+                    }
+                }
+            }
+
+            Ans = sb.ToString();
+        }
+
+        public string Ans
+        {
+            get => ans;
+            set
+            {
+                if (ans != value)
+                {
+                    ans = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public int Up
+        {
+            get => up;
+            set
+            {
+                if (up != value)
+                {
+                    up = value;
+                    PrimeSearch();
+                }
+            }
+        }
+
+        public int Down
+        {
+            get => down;
+            set
+            {
+                if (down != value)
+                {
+                    down = value;
+                    PrimeSearch();
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
