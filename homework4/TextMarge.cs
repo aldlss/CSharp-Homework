6e4d68d4ebd4b6bc08b19d402b5fc6d758e3a0c8
aldlss <ayaldlss@gmail.com>
2023-03-27 19:12:33 +0800

    add: add homework4 half
    架构崩了，刚请教了一下 new Bing MVVM 怎么写，准备重构

---
+++ homework4/TextMarge.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace homework4
+{
+    class TextMarge : INotifyPropertyChanged, ICommand
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public event EventHandler? CanExecuteChanged;
+        private string _result;
+
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (parameter is null)
+                return false;
+            var textSelects = (object[])parameter;
+            if (textSelects.Length != 2)
+                return false;
+            return ((TextSelect)textSelects[0]).IsExist == Visibility.Hidden && ((TextSelect)textSelects[1]).IsExist == Visibility.Hidden;
+        }
+
+        public async void Execute(object? parameter)
+        {
+            
+        }
+
+        private async Task<string> DoTextMarge(string path1, string path2, string outFilePath)
+        {
+            try
+            {
+                var file1 = File.OpenText(path1);
+                var file2 = File.OpenText(path2);
+                var file1Text = await file1.ReadToEndAsync();
+                var file2Text = await file2.ReadToEndAsync();
+
+                var directoryPath = Path.GetDirectoryName(outFilePath);
+                if (Directory.Exists(directoryPath))
+                    Directory.CreateDirectory(directoryPath);
+
+                var fileOut = File.CreateText(outFilePath);
+                await fileOut.WriteLineAsync("文件 1 内容如下：");
+                await fileOut.WriteAsync(file1Text);
+                await fileOut.WriteLineAsync("\n文件 2 内容如下");
+                await fileOut.WriteAsync(file2Text);
+
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ homework4/TextSelect.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace homework4
+{
+    internal class TextSelect : INotifyPropertyChanged, ICommand
+    {
+        private string _path;
+        private Visibility _isExist;
+
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                { 
+                    _path = value;
+                    OnPropertyChanged();
+                    IsExist = File.Exists(_path) ? Visibility.Hidden : Visibility.Visible;
+                }
+            }
+        }
+
+        public Visibility IsExist
+        {
+            get => _isExist;
+            set
+            {
+                if (_isExist != value)
+                {
+                    _isExist = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public TextSelect()
+        {
+            _path = string.Empty;
+            _isExist = Visibility.Hidden;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                ShowReadOnly = true,
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                Path = dialog.FileName;
+            }
+        }
+    }
+}
