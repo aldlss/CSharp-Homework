9680b7ed5d8d65553ed0a7c5a97f887524cd3345
aldlss <ayaldlss@gmail.com>
2023-05-19 23:07:01 +0800

    test: add homework8 test
    主要是学习一下，顺手就拿一个来练练了，由于写得挺一坨的，感觉不太好写测试……

---
+++ homework8/homework8Test/ViewModelTest.cs
@@ -0,0 +1,21 @@
+﻿using homework8.viewModel;
+
+namespace homework8Test
+{
+    public class ViewModelTest
+    {
+        [Fact]
+        public void DbPathTest()
+        {
+            Environment.SetEnvironmentVariable("WORDS_DB_PATH", "123");
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                var _ = new ViewModel();
+            });
+
+            Environment.SetEnvironmentVariable("WORDS_DB_PATH", "F:\\code\\homework\\softwareStructBase\\homework8\\words.db");
+            var viewModel = new ViewModel();
+            Assert.NotNull(viewModel);
+        }
+    }
+}
